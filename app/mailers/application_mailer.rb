2c948882d71ad8a943005fde497ea87ba29628c3
Scott <scottmail88@gmail.com>
2017-06-08 03:22:50 +0000

    Add action mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,13 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "scottmail88@gmail.com"
   layout 'mailer'
+  ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  :domain         => 'swiki1.heroku.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
 end
