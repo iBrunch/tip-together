8abc381ae748c409892d74c64baf7935eefde9ad
Scott Arakawa <scottmail88@gmail.com>
2017-07-10 01:24:16 -0500

    Add sandbox domain

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,7 +5,7 @@ if Rails.env.development? || Rails.env.production?
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'sandboxeb7efa2fc9ed4ecb801fe4c5e6ff8691.mailgun.org',
     enable_starttls_auto: true,
     :authentication => :plain,
     }
